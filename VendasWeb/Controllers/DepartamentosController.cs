29cc717f6f1279145e46173f08ec18e36e35e077
Renato Nakano <renato.sousa.gc@hotmail.com>
2020-05-19 00:08:08 -0300

    Criacao do MVC: Departamento

---
+++ VendasWeb/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VendasWeb.Models;
+
+namespace VendasWeb.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List <Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Name = "Eletronics" });
+            list.Add(new Departamento { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
