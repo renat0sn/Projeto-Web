bb4ac098b7eabd4703d2dc7ab60d9e46aec8b0b9
Renato Nakano <renato.sousa.gc@hotmail.com>
2020-05-19 00:18:17 -0300

    CRUD scaffold

---
+++ VendasWeb/Data/VendasWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VendasWeb.Models;
+
+namespace VendasWeb.Data
+{
+    public class VendasWebContext : DbContext
+    {
+        public VendasWebContext (DbContextOptions<VendasWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWeb.Models.Departamento> Departamento { get; set; }
+    }
+}
