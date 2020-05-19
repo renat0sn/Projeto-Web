937862c024765c7be317c80352d923358c0ad443
Renato Nakano <renato.sousa.gc@hotmail.com>
2020-05-19 01:00:59 -0300

    Migracao do SqlServer para o MySql

--- VendasWeb/Startup.cs
+++ VendasWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace VendasWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<VendasWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("VendasWebContext")));
+                    options.UseMySql(Configuration.GetConnectionString("VendasWebContext"), builder =>
+                        builder.MigrationsAssembly("VendasWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
